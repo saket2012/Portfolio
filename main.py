01a5e77963831e1c3e9142261a55bd9a3e78a9e8
Saket Gonte <saketgonte@Sakets-MacBook-Air.local>
2018-10-18 19:09:24 -0700

    Added project

---
+++ main.py
@@ -0,0 +1,26 @@
+import time, traceback, sys, logging,json,os
+from flask import Flask,request,render_template,url_for,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("main.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
